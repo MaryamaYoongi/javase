01c5062ea57c01c8584a5791b2f44aa2701873a3
MaryamaYoongi <43704369+MaryamaYoongi@users.noreply.github.com>
2018-11-17 18:25:45 +0300

    Add files via upload

---
+++ Controller.java
@@ -0,0 +1,15 @@
+package login;
+
+import javafx.fxml.FXML;
+
+public class Controller {
+    @FXML
+    public void outIsClicked() {
+        System.out.println("Кнопка *Выйти* была нажата.");
+    }
+
+    public void addAkk(){
+        System.out.println("Кнопка *Добавить аккаунт* была нажата.");
+    }
+
+}
